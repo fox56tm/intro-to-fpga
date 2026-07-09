2179b225820194982bb1cf883fafd81afec200be
Dmitry Sergeev <dmitrijsergeev111@gmail.com>
2026-07-09 18:40:51 +0300

    wip module 03

---
+++ src/03-principles-of-constructor/1_exer/peirce_arr_disj.sv
@@ -0,0 +1,26 @@
+module peirce_arr_disj (
+    input logic a,
+    input logic b, 
+    output logic c_out
+);
+    logic not_a, not_b;
+
+    peirce_arrow p_arr1 (
+        .a(a),
+        .b(b),
+        .c_out(not_a)
+    );
+
+    peirce_arrow p_arr2 (
+        .a(a),
+        .b(b),
+        .c_out(not_b)
+    );
+
+    peirce_arrow p_arr3 (
+        .a(not_a),
+        .b(not_b),
+        .c_out(c_out)
+    );
+
+endmodule
---
+++ src/03-principles-of-constructor/1_exer/peirce_arr_implic.sv
@@ -0,0 +1,26 @@
+module peirce_arr_implic (
+    input logic a,
+    input logic b, 
+    output logic c_out
+);
+    logic temp1, temp2;  
+
+    peirce_arrow pa1 ( // temp1 = ~a, temp2 = (a & ~b), =>
+        .a(a),           // => temp3 = ~temp2 & ~temp2 =
+        .b(b),           // = (~a | b) & (~a | b)
+        .c_out(temp1)
+    );
+
+    peirce_arrow pa2 (
+        .a(temp1),
+        .b(b),
+        .c_out(temp2)
+    );
+
+    peirce_arrow pa3 ( 
+    .a(temp2),
+    .b(temp2),
+    .c_out(c_out)
+    );
+
+endmodule

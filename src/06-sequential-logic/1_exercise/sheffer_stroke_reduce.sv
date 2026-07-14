02d4afedf62419c3db363d5b13551d2a6e8ee0bc
Dmitry Sergeev <dmitrijsergeev111@gmail.com>
2026-07-15 02:15:34 +0300

    finish exercises 1-3

---
+++ src/06-sequential-logic/1_exercise/sheffer_stroke_reduce.sv
@@ -0,0 +1,33 @@
+module sheffer_stroke_reduce #(
+  parameter int COUNT_OF_BITS = 4
+  )(
+    input logic [COUNT_OF_BITS-1:0] vector,
+    input logic clk,
+    input logic rst,
+    output logic result
+  );
+    logic stroke_out;
+    logic acc;
+    logic bit_in;
+    logic[COUNT_OF_BITS-1:0] temp_vector;
+    assign bit_in = temp_vector[0];
+    assign result = acc;
+
+    sheffer_stroke stroke(
+      .a(acc),
+      .b(bit_in),
+      .c_out(stroke_out)
+    );
+
+    always_ff @(posedge clk) begin
+      if(rst)begin
+        acc <= 1'b1;
+        temp_vector <= vector;
+      end
+      else begin
+        acc <= stroke_out;
+        temp_vector <= temp_vector >> 1;
+      end
+    end
+    
+endmodule
